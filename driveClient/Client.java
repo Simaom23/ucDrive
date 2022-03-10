7e7684ab9dcedc0b5b89d50a22d61844449bc6a2
Simaom23 <simaocm23@gmail.com>
2022-03-10 11:13:28 +0000

    Added config files

--- driveClient/Client.java
+++ driveClient/Client.java
@@ -1,39 +1,54 @@
 package driveClient;
 
-import java.net.MulticastSocket;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
 
-public class Client extends Thread {
-    private String MULTICAST_ADDRESS = "224.3.2.1";
-    private int PORT = 4321;
+public class Client {
+    private static int serversocket = 6000;
 
-    public static void main(String[] args) {
-        Client client = new Client();
-        client.start();
-    }
+    public static void main(String args[]) {
+        // args[0] <- hostname of destination
+        if (args.length == 0) {
+            System.out.println("java Client hostname");
+            System.exit(0);
+        }
 
-    public void run() {
-        MulticastSocket socket = null;
-        try {
-            socket = new MulticastSocket(PORT); // create socket and bind it
-            InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-            socket.joinGroup(group);
-            while (true) {
-                byte[] buffer = new byte[256];
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(packet);
+        // Socket creation
+        try (Socket s = new Socket(args[0], serversocket)) {
+            System.out.println("### - Connected to ucDrive - ####");
 
-                System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":"
-                        + packet.getPort() + " with message:");
-                String message = new String(packet.getData(), 0, packet.getLength());
-                System.out.println(message);
+            // Input and output stream
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            try (Scanner sc = new Scanner(System.in)) {
+                while (true) {
+                    // Get username
+                    System.out.print("Username: ");
+                    String username = sc.nextLine();
+
+                    // Get password
+                    System.out.print("Password: ");
+                    String password = sc.nextLine();
+
+                    // Send username and password to server
+                    out.writeUTF(username);
+                    out.writeUTF(password);
+
+                    // Server auth
+                    String auth = in.readUTF();
+                    if (auth == "true")
+                        break;
+                }
             }
+
+        } catch (UnknownHostException e) {
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            socket.close();
+            System.out.println("IO:" + e.getMessage());
         }
     }
 }
--- driveServer/Server.java
+++ driveServer/Server.java
@@ -1,47 +1,73 @@
 package driveServer;
 
-import java.net.MulticastSocket;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.io.IOException;
+// TCPServer2.java: Multithreaded server
+import java.net.*;
+import java.util.LinkedList;
+import java.io.*;
 
-public class Server extends Thread {
-    private String MULTICAST_ADDRESS = "224.3.2.1";
-    private int PORT = 4321;
-    private long SLEEP_TIME = 5000;
+public class Server {
+    private static int serverPort = 6000;
 
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.start();
-    }
+    public static void main(String args[]) {
+        int numero = 0;
+        Socket[] socketList = new Socket[2];
 
-    public Server() {
-        super("Server " + (long) (Math.random() * 1000));
-    }
-
-    public void run() {
-        MulticastSocket socket = null;
-        long counter = 0;
-        System.out.println(this.getName() + " running...");
-        try {
-            socket = new MulticastSocket(); // create socket without binding it (only for sending)
+        try (ServerSocket listenSocket = new ServerSocket(serverPort)) {
+            System.out.println("A escuta no porto 6000");
+            System.out.println("LISTEN SOCKET=" + listenSocket);
             while (true) {
-                String message = this.getName() + " packet " + counter++;
-                byte[] buffer = message.getBytes();
-
-                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
-                socket.send(packet);
-
-                try {
-                    sleep((long) (Math.random() * SLEEP_TIME));
-                } catch (InterruptedException e) {
-                }
+                Socket clientSocket = listenSocket.accept(); // BLOQUEANTE
+                socketList[numero] = clientSocket;
+                System.out.println("CLIENT_SOCKET (created at accept())=" + clientSocket);
+                numero++;
+                new Connection(clientSocket, numero, socketList);
             }
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            socket.close();
+            System.out.println("Listen:" + e.getMessage());
+        }
+    }
+}
+
+// = Thread para tratar de cada canal de comunicação com um cliente
+class Connection extends Thread {
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+    int thread_number;
+    Socket[] socketL;
+
+    public Connection(Socket aClientSocket, int numero, Socket[] socketList) {
+        thread_number = numero;
+        try {
+            clientSocket = aClientSocket;
+            socketL = socketList;
+            in = new DataInputStream(clientSocket.getInputStream());
+            this.start();
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    // =============================
+    public void run() {
+        String resposta;
+        try {
+            while (true) {
+                // an echo server
+                String data = in.readUTF();
+                resposta = "T[" + thread_number + "]: " + data;
+                System.out.println("T[" + thread_number + "] Recebeu: " + data);
+                for (int i = 0; i < socketL.length; i++) {
+                    if (i != thread_number - 1) {
+                        out = new DataOutputStream(socketL[i].getOutputStream());
+                        out.writeUTF(resposta);
+                    }
+                }
+            }
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e);
+        } catch (IOException e) {
+            System.out.println("IO:" + e);
         }
     }
 }
