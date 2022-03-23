102f79da04a2f9d9c6d01d3f5e4c35428f3f93a8
Simaom23 <simaocm23@gmail.com>
2022-03-23 20:46:32 +0000

    Added functions and listUserFiles

--- driveClient/Client.java
+++ driveClient/Client.java
@@ -9,6 +9,9 @@ public class Client {
     private static String serverhostname = "0.0.0.0";
     public static final String GREEN = "\u001B[32m";
     public static final String RESET = "\u001B[0m";
+    public static DataInputStream in;
+    public static DataOutputStream out;
+    public static String currentDir = System.getProperty("user.dir");
 
     public static void main(String args[]) {
         // Socket creation
@@ -16,53 +19,36 @@ public class Client {
             System.out.println("Welcome to ucDrive 1.0");
 
             // Input and output stream
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            in = new DataInputStream(s.getInputStream());
+            out = new DataOutputStream(s.getOutputStream());
 
             try (Scanner sc = new Scanner(System.in)) {
                 while (true) {
-                    // Get username
-                    System.out.print("Username: ");
-                    String username = sc.nextLine();
-                    out.writeUTF(username);
+                    String checkAuth = in.readUTF();
+                    if (checkAuth.equals("false"))
+                        authentication(sc);
 
-                    // Get password
-                    System.out.print("Password: ");
-                    String password = sc.nextLine();
-                    out.writeUTF(password);
+                    String currentDir = in.readUTF();
+                    System.out.print(currentDir + "> ");
 
-                    // Server auth
-                    String auth = in.readUTF();
-                    if (auth.equals("true")) {
-                        System.out.println("\nAuthenticated!");
-                        break;
-                    } else
-                        System.out.println("Not valid!\n");
-                }
-
-                String currentDir = in.readUTF();
-                System.out.print(currentDir + "> ");
-                while (true) {
                     String command = sc.nextLine();
                     out.writeUTF(command);
                     switch (command) {
+                        case "passwd":
+                            changePasswd(sc);
+                            break;
+
                         case "ls":
-                            String files = in.readUTF();
-                            String[] fileList = files.split(" ");
-                            for (String file : fileList) {
-                                if (file.charAt(0) == '/')
-                                    System.out.print(GREEN + file.substring(1) + RESET + "\t");
-                                else
-                                    System.out.print(file + "\t");
-                            }
-                            System.out.println();
+                            listDriveFiles(sc);
                             break;
 
                         case "cd":
                             break;
+
+                        case "ls -p":
+                            listUserFiles(sc);
+                            break;
                     }
-                    currentDir = in.readUTF();
-                    System.out.print(currentDir + "> ");
                 }
             }
 
@@ -73,5 +59,85 @@ public class Client {
         } catch (IOException e) {
             System.out.println("IO:" + e.getMessage());
         }
+    }
+
+    private static void authentication(Scanner sc) {
+        try {
+            while (true) {
+                // Get username
+                System.out.print("Username: ");
+                String username = sc.nextLine();
+                out.writeUTF(username);
+
+                // Get password
+                System.out.print("Password: ");
+                String password = sc.nextLine();
+                out.writeUTF(password);
+
+                // Server auth
+                String auth = in.readUTF();
+                if (auth.equals("true")) {
+                    System.out.println("\nAuthenticated!");
+                    break;
+                } else
+                    System.out.println("Not valid!\n");
+            }
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        }
+    }
+
+    private static void changePasswd(Scanner sc) {
+        try {
+            System.out.print("Enter current Password: ");
+            String password = sc.nextLine();
+            out.writeUTF(password);
+            String check = in.readUTF();
+            if (check.equals("true")) {
+                System.out.print("Enter new Password: ");
+                password = sc.nextLine();
+                out.writeUTF(password);
+                System.out.print("Retype new Password: ");
+                password = sc.nextLine();
+                out.writeUTF(password);
+            }
+            String changePass = in.readUTF();
+            System.out.println(changePass);
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        }
+    }
+
+    private static void listDriveFiles(Scanner sc) {
+        try {
+            String files = in.readUTF();
+            String[] fileList = files.split(" ");
+            for (String file : fileList) {
+                if (file.charAt(0) == '/')
+                    System.out.print(GREEN + file.substring(1) + RESET + "\t");
+                else
+                    System.out.print(file + "\t");
+            }
+            System.out.println();
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        }
+    }
+
+    private static void listUserFiles(Scanner sc) {
+        File cur = new File(currentDir);
+        String[] fileList = cur.list();
+        for (int i = 0; i < fileList.length; i++) {
+            if (new File(currentDir + "/" + fileList[i])
+                    .isDirectory())
+                fileList[i] = "/" + fileList[i];
+        }
+        for (String file : fileList) {
+            if (file.charAt(0) == '/')
+                System.out.print(GREEN + file.substring(1) + RESET + "\t");
+            else
+                System.out.print(file + "\t");
+        }
+        System.out.println();
     }
 }
--- driveServer/Server.java
+++ driveServer/Server.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 // Primary server
 public class Server {
     private static int serverPort = 6000;
-    private static String usersFile = "driveServer/users.properties";
+    public static String usersFile = "driveServer/users.properties";
     public static Properties users = new Properties();
 
     public static void main(String args[]) {
@@ -17,6 +17,7 @@ public class Server {
             System.out.println("### - ucDrive Server Info - ###");
             try (InputStream in = new FileInputStream(usersFile)) {
                 Server.users.load(in);
+                in.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -39,6 +40,7 @@ class Connection extends Thread {
     DataOutputStream out;
     Socket clientSocket;
     int thread_number;
+    boolean auth = false;
     Properties users = Server.users;
     String username;
     String currentDir = "home";
@@ -58,6 +60,41 @@ class Connection extends Thread {
     public void run() {
         try {
             while (true) {
+                if (auth)
+                    out.writeUTF("true");
+                else {
+                    out.writeUTF("false");
+                    authentication();
+                }
+
+                out.writeUTF(currentDir);
+
+                String command = in.readUTF();
+                switch (command) {
+                    case "passwd":
+                        changePasswd();
+                        break;
+
+                    case "ls":
+                        listFiles();
+                        break;
+
+                    case "cd":
+                        if (new File("driveServer/Users/" + currentDir).isDirectory())
+                            return;
+                        break;
+                }
+            }
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e);
+        } catch (IOException e) {
+            System.out.println("IO:" + e);
+        }
+    }
+
+    private void authentication() {
+        try {
+            while (true) {
                 username = in.readUTF();
                 String password = in.readUTF();
 
@@ -65,39 +102,65 @@ class Connection extends Thread {
                         && password.equals(users.getProperty(username + ".password"))) {
                     out.writeUTF("true");
                     break;
-                } else {
+                } else
                     out.writeUTF("false");
-                }
             }
 
             File home = new File("driveServer/Users/" + username + "/home");
             if (!home.exists() && !home.isDirectory())
                 home.mkdirs();
 
-            out.writeUTF(currentDir);
+            auth = true;
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e);
+        } catch (IOException e) {
+            System.out.println("IO:" + e);
+        }
+    }
 
-            while (true) {
-                String command = in.readUTF();
-                switch (command) {
-                    case "ls":
-                        File dir = new File("driveServer/Users/" + username + "/" + currentDir);
-                        String[] fileList = dir.list();
-                        String directories = "";
-                        for (int i = 0; i < fileList.length; i++) {
-                            if (new File("driveServer/Users/" + username + "/" + currentDir + "/" + fileList[i])
-                                    .isDirectory()) {
-                                fileList[i] = "/" + fileList[i];
-                            }
-                            directories += fileList[i] + " ";
-                        }
-                        out.writeUTF(directories);
-                        break;
-
-                    case "cd":
-                        break;
-                }
-                out.writeUTF(currentDir);
+    private void changePasswd() {
+        try {
+            String currentPasswd = in.readUTF();
+            if (currentPasswd.equals(users.getProperty(username + ".password"))) {
+                out.writeUTF("true");
+                String newPasswd = in.readUTF();
+                String rePasswd = in.readUTF();
+                if (newPasswd.equals(rePasswd))
+                    try (OutputStream o = new FileOutputStream(Server.usersFile)) {
+                        users.setProperty(username + ".password", newPasswd);
+                        users.store(o, null);
+                        o.close();
+                        out.writeUTF("passwd: password updated successfully");
+                        auth = false;
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                else
+                    out.writeUTF("passwd: passwords do not match");
+            } else {
+                out.writeUTF("false");
+                out.writeUTF("passwd: wrong password");
             }
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e);
+        } catch (IOException e) {
+            System.out.println("IO:" + e);
+        }
+    }
+
+    private void listFiles() {
+        try {
+            File dir = new File("driveServer/Users/" + username + "/" + currentDir);
+            String[] fileList = dir.list();
+            String directories = "";
+            for (int i = 0; i < fileList.length; i++) {
+                if (new File("driveServer/Users/" + username + "/" + currentDir + "/" + fileList[i])
+                        .isDirectory()) {
+                    fileList[i] = "/" + fileList[i];
+                }
+                directories += fileList[i] + " ";
+            }
+            out.writeUTF(directories);
         } catch (EOFException e) {
             System.out.println("EOF:" + e);
         } catch (IOException e) {
