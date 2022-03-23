f3f734d53ec4776e95f708e8e73fb743d9f2a9ad
Simaom23 <simaocm23@gmail.com>
2022-03-23 15:42:07 +0000

    Added list files and auth

--- driveClient/Client.java
+++ driveClient/Client.java
@@ -7,11 +7,13 @@ import java.io.*;
 public class Client {
     private static int serversocket = 6000;
     private static String serverhostname = "0.0.0.0";
+    public static final String GREEN = "\u001B[32m";
+    public static final String RESET = "\u001B[0m";
 
     public static void main(String args[]) {
         // Socket creation
         try (Socket s = new Socket(serverhostname, serversocket)) {
-            System.out.println("### - Connected to ucDrive - ####");
+            System.out.println("Welcome to ucDrive 1.0");
 
             // Input and output stream
             DataInputStream in = new DataInputStream(s.getInputStream());
@@ -31,10 +33,36 @@ public class Client {
 
                     // Server auth
                     String auth = in.readUTF();
-                    if (auth == "true")
+                    if (auth.equals("true")) {
+                        System.out.println("\nAuthenticated!");
                         break;
-                    else
-                        System.out.print("Not valid!");
+                    } else
+                        System.out.println("Not valid!\n");
+                }
+
+                String currentDir = in.readUTF();
+                System.out.print(currentDir + "> ");
+                while (true) {
+                    String command = sc.nextLine();
+                    out.writeUTF(command);
+                    switch (command) {
+                        case "ls":
+                            String files = in.readUTF();
+                            String[] fileList = files.split(" ");
+                            for (String file : fileList) {
+                                if (file.charAt(0) == '/')
+                                    System.out.print(GREEN + file.substring(1) + RESET + "\t");
+                                else
+                                    System.out.print(file + "\t");
+                            }
+                            System.out.println();
+                            break;
+
+                        case "cd":
+                            break;
+                    }
+                    currentDir = in.readUTF();
+                    System.out.print(currentDir + "> ");
                 }
             }
 
--- driveServer/Server.java
+++ driveServer/Server.java
@@ -2,20 +2,29 @@ package driveServer;
 
 import java.net.*;
 import java.io.*;
+import java.util.Properties;
 
 // Primary server
 public class Server {
     private static int serverPort = 6000;
+    private static String usersFile = "driveServer/users.properties";
+    public static Properties users = new Properties();
 
     public static void main(String args[]) {
         int num = 0;
         try (ServerSocket listenSocket = new ServerSocket(serverPort)) {
             System.out.println("Listening On -> " + listenSocket);
-            System.out.println("### - Server Info - ###");
+            System.out.println("### - ucDrive Server Info - ###");
+            try (InputStream in = new FileInputStream(usersFile)) {
+                Server.users.load(in);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
             while (true) {
                 Socket clientSocket = listenSocket.accept(); // BLOQUEANTE
-                System.out.println("New Connection -> " + clientSocket);
                 num++;
+                System.out.println("[" + num + "] " + "New Connection -> " + clientSocket);
                 new Connection(clientSocket, num);
             }
         } catch (IOException e) {
@@ -30,6 +39,9 @@ class Connection extends Thread {
     DataOutputStream out;
     Socket clientSocket;
     int thread_number;
+    Properties users = Server.users;
+    String username;
+    String currentDir = "home";
 
     public Connection(Socket aClientSocket, int num) {
         try {
@@ -46,8 +58,45 @@ class Connection extends Thread {
     public void run() {
         try {
             while (true) {
-                String username = in.readUTF();
+                username = in.readUTF();
                 String password = in.readUTF();
+
+                if (users.getProperty(username + ".password") != null
+                        && password.equals(users.getProperty(username + ".password"))) {
+                    out.writeUTF("true");
+                    break;
+                } else {
+                    out.writeUTF("false");
+                }
+            }
+
+            File home = new File("driveServer/Users/" + username + "/home");
+            if (!home.exists() && !home.isDirectory())
+                home.mkdirs();
+
+            out.writeUTF(currentDir);
+
+            while (true) {
+                String command = in.readUTF();
+                switch (command) {
+                    case "ls":
+                        File dir = new File("driveServer/Users/" + username + "/" + currentDir);
+                        String[] fileList = dir.list();
+                        String directories = "";
+                        for (int i = 0; i < fileList.length; i++) {
+                            if (new File("driveServer/Users/" + username + "/" + currentDir + "/" + fileList[i])
+                                    .isDirectory()) {
+                                fileList[i] = "/" + fileList[i];
+                            }
+                            directories += fileList[i] + " ";
+                        }
+                        out.writeUTF(directories);
+                        break;
+
+                    case "cd":
+                        break;
+                }
+                out.writeUTF(currentDir);
             }
         } catch (EOFException e) {
             System.out.println("EOF:" + e);
